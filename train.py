ba66a5a549f5b6e7e21c546eb369e82096a5ad94
Adamdad <787707188@qq.com>
2020-02-08 16:17:07 -0800

    commit

--- train.py
+++ train.py
@@ -6,8 +6,8 @@ device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 class Train:
 	def __init__(self, batch_size, epochs=1, method="cross-entropy", num_classes=10):
 		model = UNet(num_classes).to(device)
-		x = torch.zeros(batch_size, 3, 1024, 2048, device=device)
-		y =	torch.empty(batch_size, 1024, 2048, dtype=torch.long).random_(num_classes).to(device)
+		x = torch.zeros(batch_size, 3, 512, 1024, device=device)
+		y =	torch.empty(batch_size, 512, 1024, dtype=torch.long).random_(num_classes).to(device)
 		out = model(x)
 		if method == "cross-entropy":
 			criterio = nn.CrossEntropyLoss()
